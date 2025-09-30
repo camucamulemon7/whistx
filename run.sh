bd765c3d9c27978239c5645a7ea7575152bb352f
camucamulemon7 <botbotbot1129@gmail.com>
2025-10-01 00:52:55 +0900

    rename

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # docker compose stop
 # docker compose rm -f
 docker compose build --no-cache
-docker compose up --force-recreate 
-# docker compose -f docker-compose.yaml -f docker-compose.override.yaml up --force-recreate --detach
+docker compose -f docker-compose.yaml up --force-recreate
+# docker compose -f docker-compose.yaml up --force-recreate --detach
